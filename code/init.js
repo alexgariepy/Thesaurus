194ebc7fced0901ab548155dd01ad6328ffa022d
alexgariepy <37715390+alexgariepy@users.noreply.github.com>
2019-05-24 15:17:41 -0400

    Add files via upload

--- code/init.js
+++ code/init.js
@@ -480,22 +480,3 @@ function chronoReset() {
     document.getElementById("chronotime").innerHTML = "00:00"
     start = new Date()
 }
-
-var i = 0;
-
-function increment() {
-    if (booDecremente) {
-        if (score >= 50) {
-            booScore = true;
-            score -= 50;
-            document.getElementById("score").innerHTML = "Score : " + (score);
-        } else {
-            revenirSol(camera);
-            if (booScore) {
-                dessiner(objgl, objProgShaders, objScene3D);
-                booScore = false;
-            }
-        }
-    }
-    clearInterval(interval);
-}
